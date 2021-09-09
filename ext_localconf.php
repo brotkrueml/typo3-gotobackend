93ea13cec2452b65e9bd2d104c158b554fa5f5e9
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2021-09-09 20:27:52 +0200

    [TASK] Use constant for module key

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,7 @@
 <?php
 defined('TYPO3') or die();
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['adminpanel']['modules']['ext-gotobackend'] = [
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['adminpanel']['modules'][Brotkrueml\GoToBackend\Extension::ADMINPANEL_MODULE_IDENTIFIER] = [
     'module' => Brotkrueml\GoToBackend\AdminPanel\EditModule::class,
     'after' => ['cache'],
 ];
